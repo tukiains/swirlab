f48ab87023b49f2b9f9c817cd400323084e95b30
Simo Tukiainen <tukiains@gmail.com>
2016-03-29 13:56:30 +0300

    faster reading of ftir spectrum

--- src/read_ftir_spectrum.m
+++ src/read_ftir_spectrum.m
@@ -1,12 +1,15 @@
 function [refe,refe_wn] = read_ftir_spectrum(fname,wnrange);
 % [refe,refe_wl] = read_ftir_spectrum(fname,wnrange)
 
-a = textread(fname);
-refe_wn = a(:,1); 
-refe = a(:,2);
+fid = fopen(fname);
+data = textscan(fid, '%f64%f64');
+
+refe_wn = data{1}; 
+refe = data{2};
 
 ind = find(refe_wn>min(wnrange) & refe_wn<max(wnrange));
 
 refe = refe(ind);
 refe_wn = refe_wn(ind);
 
+fclose(fid);
