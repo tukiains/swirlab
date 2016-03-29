32065c3e5d2311ed233a697a08a84286bd94633d
Simo Tukiainen <tukiains@gmail.com>
2016-03-29 12:43:42 +0300

    only one prior term with mcmc

--- src/dimension_reduction_retrieval/ssfun_mcmc.m
+++ src/dimension_reduction_retrieval/ssfun_mcmc.m
@@ -4,4 +4,7 @@ function r = ssfun_mcmc(params,data)
 % residual
 r = resfun_dr(params(:),data.d,data.P,data.varargin);
 
+% mcmc already has prior for the alpha parameters
+r = r(1:end-sum(data.d));
+
 r = sum(r.^2);
