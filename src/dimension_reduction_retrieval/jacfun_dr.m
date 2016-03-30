123a21493f84b1cfbc404f5f27075aadae422ecb
Simo Tukiainen <tukiains@gmail.com>
2016-03-30 12:33:49 +0300

    bug with fixed gases

--- src/dimension_reduction_retrieval/jacfun_dr.m
+++ src/dimension_reduction_retrieval/jacfun_dr.m
@@ -1,5 +1,8 @@
-function X2 = jacfun_dr(theta,d,P,varargin)
-% X2 = jacfun_dr(theta,P,d,varargin)
+function [X2 X3] = jacfun_dr(theta,d,P,varargin)
+% [J J2] = jacfun_dr(theta,P,d,varargin)
+%
+% J: [nwl k] (reduced)
+% J2: [nwl nalt] (full)
 %
 % Jacobian function for dimension reduction
 
@@ -21,32 +24,37 @@ err = weight_term(sol,noise);
 
 % gases
 X2 = [];
+X3 = [];
 for n = 1:length(invgas)
     ind = find(ismember(gasvec,invgas(n))==1);
     X = squeeze(K2(ind,:,:))';
     prof = geo.layer_dens.(char(invgas(n))).*geo.los_lens';
-    for m=1:size(X,2)
+    for m = 1:size(X,2)
         X(:,m) = conv_spectrum(wn,X(:,m));
         X(:,m) = interp1(wn,X(:,m),wn+wn_shift,'linear','extrap')./err*prof(m);
     end
     X2 = [X2 X*P{n}];
+    X3 = [X3 X];
 end
 
 % base line + offset
 z=1;
 for n = sum(d)+1:sum(d)+4
-    X2(:,n) = conv_spectrum(wn,K(:,length(invgas)+z));
+    X2(:,n) = conv_spectrum(wn,K(:,length(gasvec)+z));
     X2(:,n) = interp1(wn,X2(:,n),wn+wn_shift,'linear','extrap')./err;
-    z=z+1;
+    z = z+1;
 end
 
 % remove edges
 X2 = X2(15:end-15,:);
+X3 = X3(15:end-15,:);
 
 % regularization
 I = eye(sum(d));
 I = [I zeros(sum(d),4)];
 X2 = [X2; I];
+
+
 
 
 
--- src/dimension_reduction_retrieval/redu2full.m
+++ src/dimension_reduction_retrieval/redu2full.m
@@ -1,6 +1,10 @@
 function los_dens_out = redu2full(params,d,P,invgas,los_dens)
 % los_dens_out = redu2full(params,d,P,invgas,los_dens)
 
+% initialize
+los_dens_out = los_dens;
+
+% change retrieved gases
 for n = 1:length(invgas)    
     reduparams = params(sum(d(1:(n-1)))+1:sum(d(1:n))); 
     % Reduced profile for a gas
