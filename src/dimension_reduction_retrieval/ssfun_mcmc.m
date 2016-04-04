7327b569a83f09f731b7fa751d8969ff66eba917
Simo Tukiainen <tukiains@gmail.com>
2016-04-04 12:25:55 +0300

    rss

--- src/dimension_reduction_retrieval/ssfun_mcmc.m
+++ src/dimension_reduction_retrieval/ssfun_mcmc.m
@@ -1,4 +1,4 @@
-function r = ssfun_mcmc(params,data)
+function rss = ssfun_mcmc(params,data)
 % r = ssfun_mcmc(params,data)
 
 % residual
@@ -7,4 +7,5 @@ r = resfun_dr(params(:),data.d,data.P,data.varargin);
 % mcmc already has prior for the alpha parameters
 r = r(1:end-sum(data.d));
 
-r = sum(r.^2);
+rss = sum(r.^2);
+
