f2b99322909d66bf2e7b128b932ed9a3d495ac72
Simo Tukiainen <tukiains@gmail.com>
2016-04-04 16:17:20 +0300

    maybe remove this extra

--- src/weight_term.m
+++ src/weight_term.m
@@ -1,9 +1,10 @@
 function [err] = weight_term(sol,noise)
 
 err = noise*ones(length(sol),1);
-% add extra weigth to strong solar lines
-ind = find(sol<0.94);
-err(ind) = 0.002./sol(ind);
+
+% add extra weigth to strong solar lines?
+%ind = find(sol<0.94);
+%err(ind) = 0.002./sol(ind);
 
 
 
