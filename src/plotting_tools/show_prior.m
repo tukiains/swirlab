90ee1e9c9426ddbee88855f726c6f4b54e2af7ea
Simo Tukiainen <tukiains@gmail.com>
2016-04-04 17:43:25 +0300

    prior plot

---
+++ src/plotting_tools/show_prior.m
@@ -0,0 +1,10 @@
+function show_prior(alt,x0,air,C)
+% show_prior(alt,x0,air,C)
+%
+
+a = randn(size(C,2),10000);
+pa = C*a;
+profs = bsxfun(@times,exp(pa),x0(:)); 
+mix = bsxfun(@rdivide,profs,air(:));
+
+plot_curtain(alt,plims(mix',[0.025 0.5 0.975]),[.5 .5 .5],[.5 .5 .5]);
