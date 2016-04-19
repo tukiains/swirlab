067c2d70874414804dc509e1600f42b1bbf7712b
Simo Tukiainen <tukiains@gmail.com>
2016-04-19 17:35:31 +0300

    no noise in simu

--- src/dimension_reduction_retrieval/simulate_ftir_spectrum.m
+++ src/dimension_reduction_retrieval/simulate_ftir_spectrum.m
@@ -1,17 +1,18 @@
 function [refe sol_shift wn_shift simuatmos] = simulate_ftir_spectrum(c_wn,cros_o,c_alt,wn,gasvec,afile,sza,L,sol,noise,varargin)
 
 % a fine grid
-alt = create_layering(70,500,1.00001);
+alt = create_layering(70,100,1.001);
 
 [geo,cros] = calc_direct_geo(c_wn,cros_o,c_alt,wn,gasvec,afile,sza,alt);
 
-p1 = 0.21;
-p2 = 0.23;
-p3 = 0.20;
-offset = 2e-4;
+p1 = 0.4;
+p2 = 0.5;
+p3 = 0.5;
+offset = 1e-4;
 
 % aircore ch4 profile?
 if (nargin>10)
+
     [~,~,ac_ch4,~,~,~,~,ac_alt] = read_aircore_sounding(varargin{1});
     ac_ch4 = ac_ch4/1e9; % ppb -> mole fraction
     aci = extrapolate_ac(ac_ch4,ac_alt,geo.air,geo.center_alts);
@@ -21,16 +22,24 @@ if (nargin>10)
     if (nargin>11)
         per_alt = varargin{2};
         per = varargin{3};        
+
         [~,ind] = min(abs(per_alt-geo.center_alts));        
-        aci(ind) = aci(ind) + aci(ind)*per;
+
+        %dx = aci(ind)*per;
+        dx = per;
+        aci(ind) = aci(ind) + dx;
+        
+        simuatmos.dx = dx;
+        
     end
 
     geo.layer_dens.ch4 = aci;
-
+    
+    
 end
 
 simuatmos.ch4 = aci;
-simuamos.alt = alt;
+simuatmos.alt = geo.center_alts;
 
 % simulate spectrum
 refe = calc_direct_radiance(geo.layer_dens,geo.los_lens,gasvec, ...
@@ -40,6 +49,7 @@ refe = calc_direct_radiance(geo.layer_dens,geo.los_lens,gasvec, ...
 refe = conv_spectrum(wn,refe);
 
 % add noise
+rng(666);
 refe = refe + noise*randn(length(wn),1);
 
 sol_shift = 0;
