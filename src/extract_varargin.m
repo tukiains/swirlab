143e4ffe00de7bb10c415fcc4f7bda65821c0285
Simo Tukiainen <tukiains@gmail.com>
2016-11-23 15:07:18 +0200

    dont use cell2mat

--- src/extract_varargin.m
+++ src/extract_varargin.m
@@ -2,18 +2,18 @@ function [wn,gasvec,cros,refe,invgas,sol,wn_shift,noise,L,geo,err,offset,ncut] =
 
 a = a{:};
 
-wn = cell2mat(a(1));
-gasvec = a(2); gasvec = gasvec{1};
-cros = cell2mat(a(3));
-refe = cell2mat(a(4));
-invgas = a(5); invgas = invgas{1};
-sol = cell2mat(a(6));
-wn_shift = cell2mat(a(7));
-noise = cell2mat(a(8));
-L = cell2mat(a(9));
-geo = a(10); geo = geo{1};
-err = cell2mat(a(11));
-offset = cell2mat(a(12));
-ncut = cell2mat(a(13));
+wn = a{1};
+gasvec = a{2};
+cros = a{3};
+refe = a{4};
+invgas = a{5};
+sol = a{6};
+wn_shift = a{7};
+noise = a{8};
+L = a{9};
+geo = a{10};
+err = a{11};
+offset = a{12};
+ncut = a{13};
 
 
