60c04ab4303adb961a8586e2aad05ecb33a5fa01
Simo <simo.tukiainen@fmi.fi>
2017-02-16 12:46:34 +0200

    avek for column corrected

--- src/dimension_reduction_retrieval/avek_dr.m
+++ src/dimension_reduction_retrieval/avek_dr.m
@@ -8,19 +8,18 @@ function [A_alpha,A_layer,A_column] = avek_dr(K,P,theta,x0,los_len,err,alt,ncut,
 % alt: altitude grid
 % air: air profile to covert
 
-x = P*theta(:) + x0(:); % current profile
-
 % dt/dx
-Kv = K * diag(los_len.*air'/1e9); % -> into ppb space?
-
-S = diag(1./err(ncut:end-ncut).^2); % inverse of error covariance
+Kv = K * diag(los_len.*air'/1e9); 
+S = diag(1./err(ncut:end-ncut).^2); 
 
 % dt/da
-Ka =  K*diag(los_len.*air'/1e9)*P; % ppb space
-
+Ka =  Kv*P; % ppb space
 A_alpha = (Ka'*S*Ka+eye(length(theta)))\(Ka'*S*Kv); % AK of alpha
-
 A_layer = P*A_alpha; % layer-wise AK
 
-A_column = diff(alt)*A_layer;    
+Kv2 = K*diag(los_len); 
+A_alpha2 = (Ka'*S*Ka+eye(length(theta)))\(Ka'*S*Kv2);
+A_layer2 = diag(air'/1e9)*P*A_alpha2;
+A_column = diff(alt)*A_layer2; % be careful when interpreting this
 
+%keyboard
