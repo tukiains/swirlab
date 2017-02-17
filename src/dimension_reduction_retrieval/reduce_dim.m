86d214edb118fd80ea768529dbb7f2e19acfc3c0
Simo Tukiainen <simo.tukiainen@fmi.fi>
2017-02-17 10:05:55 +0200

    prior parameters

--- src/dimension_reduction_retrieval/reduce_dim.m
+++ src/dimension_reduction_retrieval/reduce_dim.m
@@ -8,15 +8,21 @@ for n=1:length(invgas)
     % new covariance marix for ch4 in ppb:
     
     if (strcmp(invgas(n),'ch4')==1) 
+
         l = 12;
         mu1 = 25;
-        mu2 = 3;
+        mu2 = 5;
         sigma1 = 10;
-        sigma2 = 50;
-        d1 = 3e2;
-        d2 = 10;
+        sigma2 = 5;
+        d1 = 300;
+        d2 = 30;
         Cor = create_cov(alt,mu1,mu2,sigma1,sigma2,d1,d2,l);
 
+        % ace prior
+        %ace = load('/home/tukiains/data/ace_prior');
+        %aaa = interp1(ace.alt,ace.acei,alt,'linear','extrap');
+        %Cor = double(cov(aaa'));
+        
     else
         Cor = diag(ones(length(alt),1))/1000;
     end
